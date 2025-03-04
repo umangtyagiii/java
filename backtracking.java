d160dc026f33d215ac9b849bd6c8edbd4a0c4b3f
umangtyagiii <umangtyagi1503@gmail.com>
2025-03-04 10:59:49 +0530

    some modifications

--- backtracking.java
+++ backtracking.java
@@ -8,6 +8,7 @@ public class backtracking {
     };
     int[][] path = new int[board.length][board[0].length];
     allPathPrint("", board, 0, 0, path, 1);
+    allPath("", board, 0, 0);
 }
 
 static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
@@ -46,4 +47,38 @@ static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path,
     maze[r][c] = true;
     path[r][c] = 0;
 }
+static void allPath(String p, boolean[][] maze, int r, int c) {
+    if (r == maze.length - 1 && c == maze[0].length - 1) {
+        System.out.println(p);
+        return;
+    }
+
+    if (!maze[r][c]) {
+        return;
+    }
+
+    // i am considering this block in my path
+    maze[r][c] = false;
+
+    if (r < maze.length - 1) {
+        allPath(p + 'D', maze, r+1, c);
+    }
+
+    if (c < maze[0].length - 1) {
+        allPath(p + 'R', maze, r, c+1);
+    }
+
+    if (r > 0) {
+        allPath(p + 'U', maze, r-1, c);
+    }
+
+    if (c > 0) {
+        allPath(p + 'L', maze, r, c-1);
+    }
+
+    // this line is where the function will be over
+    // so before the function gets removed, also remove the changes that were made by that function
+    maze[r][c] = true;
+}
+
 }
