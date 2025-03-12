ba13339dd105e34eea652b2ae38f2156b2f61dc1
umangtyagiii <umangtyagi1503@gmail.com>
2025-03-12 14:47:45 +0530

    tictactoe added

--- while_loop.java
+++ while_loop.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class while_loop {
     public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
         int n;
         System.out.println("Enter value:");
-        Scanner obj = new Scanner(System.in);
+        
         n = obj.nextInt();
         obj.close();
         while(n>=100){
